b32d0144649eba710341d765ebb2a57e11957a7f
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-22 09:36:12 +0300

    add method to save and get  pitches and comments from database

--- app/models.py
+++ app/models.py
@@ -1,11 +1,18 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
-    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    email=db.Column(db.String(255),unique=True,index = True)
+    # pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
     pass_secure = db.Column(db.String(225))
 
     @property
@@ -19,6 +26,10 @@ class User(db.Model):
     def verify_password(self,password):
         return check_password_hash(self.pass_secure,password)
 
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
     def __repr__(self):
         return f'User {self.username}'
 
@@ -28,9 +39,23 @@ class Pitch(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
     users = db.relationship('User',backref = 'role',lazy="dynamic")
+    description=db.Column(db.String)
+    like = db.Column(db.Integer)
+    dislike = db.Column(db.Integer)
+    comments_id =db.Column(db.Integer,db.ForeignKey("comments.id"))
 
-    def __repr__(self):
-        return f'User {self.name}'
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls):
+        '''
+        gets pitches from the database
+        '''
+        pitches=Pitch.query.all()
+        
+        return pitches
 
 
 class Comment(db.Model):
